66cb12e728e4b5ee2187c63e5e2c2108e59eec9f
Sergei <sergi.haiduk@gmail.com>
2023-10-26 00:45:26 +0300

    implemented basic authentication and session proxy to run on hosted server

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
 import json
 
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiohttp import BasicAuth
+
+from env_loading import load_env_variable
 from aiogram import Bot
 from aiogram import Dispatcher
 from aiogram.enums import ParseMode
 
-from env_loading import load_env_variable
+# hosting specification, login and password don't matter
+auth = BasicAuth(login='login', password='password')
+session = AiohttpSession(proxy=('http://proxy.server:3128', auth))
 
 BOT_TOKEN = load_env_variable("BOT_TOKEN")
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=BOT_TOKEN, session=session, parse_mode=ParseMode.HTML)  # remove the session along with the auth
 dp = Dispatcher()
 
 # region bot_config.json
